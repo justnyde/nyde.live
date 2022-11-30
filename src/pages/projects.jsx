b90f552d8ac9a3b8733de10de2800cf3d620d4b6
Just Nyde <88275041+justnyde@users.noreply.github.com>
2022-11-30 19:21:17 +0300

    😄 Projects page updated.

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -11,7 +11,7 @@ export default function Projects(){
         <title>Projects | Just Nyde</title>
 
         <div data-aos="fade-right">
-            <h1 className="text-xl sm:text-3xl font-sans text-gray-400 font-bold flex items-center gap-2"><AiOutlineShareAlt/> My Projects </h1>
+            <h1 className="text-3xl font-sans text-gray-400 font-bold flex items-center gap-2"><AiOutlineShareAlt/> My Projects </h1>
             <div className="mt-12 w-full grid grid-cols-1 gap-4 grid-flow-row auto-rows-max px-3 sm:px-0 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
                 {(Project.active.map(project => (
                     <div className="w-full bg-primary-100 rounded-xl p-2 group h-full">
@@ -36,11 +36,17 @@ export default function Projects(){
         </div>
         <br/><br/><br/>
         <div data-aos="fade-right">
-            <h1 className="text-xl sm:text-3xl font-sans text-gray-400 font-bold flex items-center gap-1">Where is this website <BiQuestionMark size="45px"/> </h1>
-            <div className="w-full h-45 rounded-xl bg-primary p-6 mt-4">
-                <div className="flex items-center gap-4">
-                    <AiOutlineGithub size="40px" color="WHITE"/>
-                    <h1 className="font-sans text-white text-base">This website made by Just Nyde.</h1>
+            <h1 className="text-3xl font-sans text-gray-400 font-bold flex items-center gap-1">Where is this website <BiQuestionMark size="45px"/> </h1>
+            <div className="w-full h-45 rounded-xl bg-gradient-to-r from-primary to-blue-500 p-6 mt-4">
+                <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 items-center justify-start md:justify-between">
+                    <div className="flex items-center gap-4">
+                        <AiOutlineGithub size="40px" color="WHITE"/>
+                        <h1 className="font-sans text-white text-base">This website made by Just Nyde.</h1>
+                    </div>
+                    <a className="flex items-center gap-2 text-gray-200 hover:text-gray-300" href="https://github.com/justnyde/nyde.live" target="_blank">
+                        <HiOutlineExternalLink size="30px"/>
+                        <h1 className="font-sans text-base">Use Template.</h1>
+                    </a>
                 </div>
             </div>
         </div>
