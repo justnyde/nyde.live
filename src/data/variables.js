2357736ac5388852bb1ddb3a4af4f489e864adcd
Just Nyde <88275041+justnyde@users.noreply.github.com>
2023-08-19 19:08:57 +0300

    test

--- src/data/variables.js
+++ src/data/variables.js
@@ -5,8 +5,8 @@ export default {
     description: "My name is Just Nyde. I am 16 years old Turkish student. I interested in Software and Design. I can develop front-end and back-end websites for now.",
     about: {
         description: "Hello. My name is <span class='text-primary'>Just Nyde</span>. I am <span class='text-primary'>16 years</span> old. I am a <span class='text-primary'>3rd year student</span> and I interested in <span class='text-primary'>Software</span>. I've been coding <span class='text-primary'>since 2019</span> and I want to improve myself in this field.",
-        liveIn: "İstanbul, Küçükçekmece",
-        birthday: "16 October, 2006",
+        liveIn: "İstanbul",
+        birthday: "16 October",
         gender: "Man"
     }
 
